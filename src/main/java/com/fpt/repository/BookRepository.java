d96176bc35bbf04b8b7a37a03e3084e681ec88db
LuyenDH <luyendh@fpt.edu.vn>
2019-11-23 19:24:31 +0700

    Fix search with seeding data.

--- src/main/java/com/fpt/repository/BookRepository.java
+++ src/main/java/com/fpt/repository/BookRepository.java
@@ -3,11 +3,30 @@ package com.fpt.repository;
 import com.fpt.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "SET FOREIGN_KEY_CHECKS=0;")
+    void disableForeignKeyCheck();
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "SET FOREIGN_KEY_CHECKS=1;")
+    void enableForeignKeyCheck();
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "alter table book AUTO_INCREMENT = 1")
+    void resetIncrement();
+
     List<Book> findByName(String name);
 }
--- src/main/java/com/fpt/repository/CategoryRepository.java
+++ src/main/java/com/fpt/repository/CategoryRepository.java
@@ -2,9 +2,29 @@ package com.fpt.repository;
 
 import com.fpt.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "SET FOREIGN_KEY_CHECKS=0;")
+    void disableForeignKeyCheck();
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "SET FOREIGN_KEY_CHECKS=1;")
+    void enableForeignKeyCheck();
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "alter table category AUTO_INCREMENT = 1")
+    void resetIncrement();
+
     List<Category> findByName(String name);
 }
